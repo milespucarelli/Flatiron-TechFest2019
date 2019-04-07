18ff4e12d0ba689524161daa5686bbb11e41e575
reduviidae <wiljago@gmail.com>
2019-04-07 03:14:02 -0400

    still need to remove div on mouseout

--- content.js
+++ content.js
@@ -1,35 +1,47 @@
 const initText = "Welcome to Web Atlas. Hover over something to learn more about it."
 const infoBox = document.createElement("div");
 infoBox.innerHTML = initText;
-infoBox.style.margins = "5px";
 infoBox.style.padding = "10px";
 infoBox.style.fontSize = "18px";
 infoBox.style.letterSpacing = "2px";
-document.body.insertBefore(infoBox, document.body.firstChild);
+infoBox.style.zIndex = 9999999999999;
+infoBox.style.backgroundColor = "white";
+infoBox.style.margin = "15px";
+infoBox.style.position = "absolute";
+infoBox.style.display = "none";
+document.body.prepend(infoBox);
+
+function identifyElement(target, targetArray) {
+  infoBox.style.display = "";
+  targetArray.find(function(t){
+    if (t.includes("input") && !t.includes("button")) {
+      infoBox.innerHTML = "This is an input field, enter your information";
+      target.style.border = "3px blue dotted";
+    } else if (t.includes("nav")) {
+      infoBox.innerHTML = "This is a navigation element. It helps you move around the website.";
+      target.style.border = "3px purple dotted";
+    } else if (t.includes("ads") || t.includes("promo")) {
+      infoBox.innerHTML = "This is an advertisement. You should ignore it."
+      target.style.border = "3px red dotted";
+    } else if (t.includes("button")) {
+      infoBox.innerHTML = "This is a button you can click."
+      target.style.border = "3px green dotted";
+    } else if (t === "a") {
+      infoBox.innerHTML = "This is a link. Clicking on this will redirect you."
+      target.style.border = "3px yellow dotted";
+    }
+  })
+}
 
 
 document.addEventListener("mouseover", function(e){
-  switch(e.target.tagName.toLowerCase()){
-    case "input":
-      e.target.style.border = "20px red solid";
-      e.target.classList.add("tooltip");
-      infoBox.innerHTML = "This is an input field, enter your information";
-      infoBox.style.display = "block";
-    case "nav":
-
-    default:
-        return null;
-  }
+  let targetArray = [e.target.tagName.toLowerCase(), e.target.className, e.target.id]
+  // console.log("this is what we will put into the identify function", targetArray)
+  identifyElement(e.target, targetArray)
 });
 
 
 document.addEventListener("mouseout", function(e){
-  switch(e.target.tagName.toLowerCase()){
-    case "input":
-        e.target.style.border = "0px";
-        e.target.classList.remove("tooltip");
-        infoBox.innerHTML = initText
-    default:
-        return null;
-  }
+  infoBox.style.display = "none";
+  e.target.style.border = "0px";
 })
